ea8dd0b9c0de5843db2405bfa47140777a46db1d
Mario López Baratas <mariolopez000@hotmail.com>
2016-01-15 23:15:56 +0100

    New project mvvm example demo

---
+++ MVVMExampleDemo/ViewModels/Base/ViewModelLocator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMExampleDemo.ViewModels.Base
+{
+    public class ViewModelLocator
+    {
+        //readonly IUnityContainer _container;
+
+        //public ViewModelLocator()
+        //{
+        //    _container = new UnityContainer();
+        //    #region Viewmodels
+        //    _container.RegisterType<MainPageViewModel>();
+        //    #endregion
+        //}
+
+        //public MainPageViewModel MainPageViewModel
+        //{
+        //    get { return _container.Resolve<MainPageViewModel>(); }
+        //}
+
+    }
+}
